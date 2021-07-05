90403ad95d99873fc235cb89f4984f698c25c0de
Davi Rodrigo <42841669+Davirshark@users.noreply.github.com>
2021-07-05 20:07:53 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,48 @@
+import React, { useState, useMemo, useEffect } from "react";
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+
+export default function App() {
+  const [senha, setSenha] = useState<string>('')
+  const [email, setEmail] = useState<string>('')
+
+  useEffect(() => {
+    document.title = `{senha}`
+  });
+
+  const buttonSenha = useMemo(() => {
+    if (senha.length >= 10) {
+      return false
+    } else {
+      return true
+    }
+  }, [senha])
+
+  const buttonEmail = useMemo(() => {
+    if (email.length >= 1) {
+      return false
+    } else {
+      return true
+    }
+  }, [email])
+
+  return (
+    <div>
+      <div>
+        <TextField
+          value={senha}
+          onChange={(e) => { setSenha(e.target.value) }}
+        />
+        <TextField
+          value={email}
+          onChange={(e) => { setEmail(e.target.value) }}
+        />
+      </div>
+      <div>
+        <Button color="primary" disabled={buttonSenha}>senha</Button>
+        <Button color="primary" disabled={buttonEmail}>email</Button>
+      </div>
+    </div>
+  )
+}
